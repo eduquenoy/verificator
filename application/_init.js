0cce6dbe94b86198b3ca21403a9318efeb592f97
Eric DUQUENOY <eric.duquenoy@univ-littoral.fr>
2021-03-25 13:58:20 +0100

    Mise à jour

--- application/_init.js
+++
@@ -1,6 +0,0 @@
-/*
-    Configuration de l'application
-*/
-var URLTEST = "http://experimentation.univ-littoral.fr"; //URL du serveur d'hébergement
-var DOSSIERTEST = "wordpress"; //nom du dossier à tester par défaut
-
